c31fc3e6d6fd3df592a38718e9dd5a143d6f36f6
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2022-05-14 18:10:48 +0530

    layout made

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,44 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleClick = (e) => {
+    console.log(todos)
+    e.preventDefault();
+    setTodos((p) => {
+      return [...p, todo]
+    })
+  }
+
+  const removeTodo = (index) => {
+    // console.log(todos.splice(index, 1));
+    setTodos((p) => {
+      p.splice(index, 2)
+      return p
+    })
+    setTodos((p) => p);
+  }
 
   return (
     <div className="App">
-      Hello, Akshay!
+      <form className="form">
+        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} required />
+        <button onClick={handleClick} type="submit"><ion-icon name="checkmark-sharp"></ion-icon></button>
+      </form>
+      <ul className="list">
+        {
+          todos.map((t, index) => {
+            return (
+              <li key={index}>
+                {t}
+                <button onClick={() => removeTodo(index)}><ion-icon name="close-circle-outline"></ion-icon></button>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
