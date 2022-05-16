7cfa2888edc12a45a0d4d72ecd5109643e8bb745
Aryangp <91003905+Aryangp@users.noreply.github.com>
2022-05-16 15:20:45 +0530

    added sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
 export default function Header() {
+  const openSide=()=>{
+    document.getElementById("side-bar").className="sidebar open"
+  }
+  const closeSide=()=>{
+    document.getElementById("side-bar").className="sidebar"
+  }
   return (
+    <>
     <nav className="navbar">
-      <button><i className="bi bi-three-dots-vertical"></i></button>
+      <button onClick={openSide} className="button2"><i className="bi bi-three-dots-vertical"></i>
+      <aside className="sidebar " id="side-bar">
+       <button onClick={closeSide} className="sidebar-close">x</button>
+       <h2>Todo App</h2>
+       </aside>
+      </button>
       <h2>Todo App</h2>
     </nav>
+   
+ </>
   )
 }
