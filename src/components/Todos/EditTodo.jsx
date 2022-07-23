e9dccf5eeb81941057de7060e118e03413535bc7
Akshay Kumar Sharma <82942162+Akshay-Kumar-Sharma7807@users.noreply.github.com>
2022-07-23 07:27:14 +0000

    added some features to edittodo component

--- src/components/Todos/EditTodo.jsx
+++ src/components/Todos/EditTodo.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import { Drawer, Title, Group, Switch } from "@mantine/core";
+import { Drawer, Title, Group, Textarea, Switch, Paper, TextInput, ActionIcon, ThemeIcon, Checkbox, MultiSelect } from "@mantine/core";
 
-export default function EditTodo({ editMenu, setEditMenu, todo }) {
+export default function EditTodo({ editMenu, setEditMenu, todos, index, starTodo, completeTodo, deleteTodo, addToMyDay, setTitle }) {
+  const todo = todos[index];
+  console.log("todos", todos);
+
   return (
     <Drawer
       position="right"
@@ -17,12 +20,72 @@ export default function EditTodo({ editMenu, setEditMenu, todo }) {
         }
       }}
     >
-      {/* <Title order={5} mb="md">
-        General
-      </Title> */}
-      <Group>
-        <Switch size="md" label="Add to my day" name="my-day"></Switch>
-      </Group>
+      <Paper p="xs" shadow="sm" radius="md" mb="xs" withBorder>
+        <Group key={index} size="xl">
+          <Checkbox onChange={(e) => completeTodo(e, index)}
+            radius="xl"
+            size="md"
+            checked={todo.completed} />
+
+          <TextInput
+            sx={{ flex: 1 }}
+            weight="bold"
+            size="md"
+            variant="unstyled"
+            value={todo.task}
+            onChange={(e) => setTitle(index, e.target.value)}
+          ></TextInput>
+          <ActionIcon
+            color="blue"
+            variant="hover"
+            onClick={() => starTodo(index)}
+            size="lg"
+          >
+            <i className={`bi bi-star${todo.favourite ? "-fill" : ""}`} size={20} />
+            {todo.favourite}
+          </ActionIcon>
+        </Group>
+      </Paper>
+
+      <Paper shadow="sm" radius="md" p="md" mb="xs" withBorder>
+        <Switch size="md" label="Add to my day" name="my-day" checked={todo.myDay} onChange={() => addToMyDay(index)}></Switch>
+      </Paper>
+      <Paper shadow="sm" radius="md" p="xs" mb="xs" withBorder>
+        <Group >
+          {/* <ThemeIcon pd="xs" variant="light" size="lg">
+            <i className="bi bi-tag"></i>
+          </ThemeIcon> */}
+          <MultiSelect
+            sx={{ flex: 1 }}
+            data={[
+              { value: "kjsdf", label: "Work" },
+              { value: "dfg", label: "Games" },
+              { value: "afd", label: "Fun" },
+              { value: "xcvb", label: "Essentials" },
+            ]}
+            placeholder="Choose a Category"
+            searchable
+            nothingFound="Nothing found"
+            creatable
+            getCreateLabel={(query) => `+ Create ${query}`}
+            variant="unstyled"
+            // complete this function to update categories
+            // onCreate={(query) => setData((current) => [...current, query])}
+            icon={<i className="bi bi-tag" />}
+          />
+        </Group>
+      </Paper>
+
+      <Paper shadow="sm" radius="md" p="xs" mb="xs" withBorder>
+        <Textarea
+          variant='unstyled'
+          placeholder='Add note'
+        />
+      </Paper>
+      <Paper shadow="sm" radius="md" p="md" mb="xs" withBorder>
+      </Paper>
+      <Paper shadow="sm" radius="md" p="md" mb="xs" withBorder>
+      </Paper>
     </Drawer>
   )
 }
--- src/components/Todos/ListTodos.jsx
+++ src/components/Todos/ListTodos.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Group, Checkbox, Text, ActionIcon } from "@mantine/core";
+import { Group, Checkbox, Text, ActionIcon, Paper } from "@mantine/core";
 import EditTodo from "./EditTodo";
 
 export default function ListTodos({ todos, setTodos, sortFunc, filterFunc }) {
@@ -7,59 +7,99 @@ export default function ListTodos({ todos, setTodos, sortFunc, filterFunc }) {
   sortFunc = sortFunc ?? function (a, b) { return b.importance - a.importance }
   filterFunc = filterFunc ?? function () { return true }
 
-  const [currentTodo, setCurrentTodo] = useState();
+  const [currentIndex, setIndex] = useState(0);
   const [editMenu, setEditMenu] = useState(false);
 
-  const opneEditMenu = (index) => {
-    setCurrentTodo(todos[index]);
+  const openEditMenu = (index) => {
+    setIndex(index);
     setEditMenu(true);
+  }
+
+  const deleteTodo = (index) => {
+    setTodos((t) => {
+      return t.filter((_, i) => i != index)
+    })
+  }
+
+  const starTodo = (index) => {
+    setTodos(todos.map((t, i) => {
+      if (i === index) {
+        t.favourite = !todos[index].favourite;
+      }
+      return t
+    }))
+  }
+
+  const completeTodo = (e, index) => {
+    setTodos((t) => {
+      console.log(e.target.checked)
+      t[index].completed = e.target.checked
+      return [...t]
+    })
+  }
+
+  const addToMyDay = (index) => {
+    setTodos(todos.map((t, i) => {
+      if (i === index) {
+        t.myDay = !todos[index].myDay;
+      }
+      return t
+    }))
+  }
+
+  const setTitle = (index, newTitle) => {
+    setTodos(todos.map((t, i) => {
+      if (i === index) {
+        t.task = newTitle;
+      }
+      return t
+    }))
   }
 
   return (
     <>
-      <EditTodo todo={currentTodo} editMenu={editMenu} setEditMenu={setEditMenu} />
+      <EditTodo
+        addToMyDay={addToMyDay}
+        todos={todos}
+        index={currentIndex}
+        editMenu={editMenu}
+        setEditMenu={setEditMenu}
+        completeTodo={completeTodo}
+        deleteTodo={deleteTodo}
+        starTodo={starTodo}
+        setTitle={setTitle}
+      />
 
       {
         todos.sort(sortFunc).filter(filterFunc).map((todo, index) => (
-          <Group key={index} mt="xs" size="lg">
-            <Checkbox onChange={(e) => setTodos((t) => {
-              console.log(e.target.checked)
-              t[index].completed = e.target.checked
-              return [...t]
-            })}
-              radius="xl"
-              checked={todo.completed} />
+          <Paper p="sm" shadow="md" radius="md" mt="xs" >
+            <Group key={index} size="lg">
+              <Checkbox onChange={(e) => completeTodo(e, index)}
+                radius="xl"
+                checked={todo.completed} />
 
-            <Text
-              sx={{ flex: 1 }}
-              onClick={() => { opneEditMenu(index) }}
-            >{todo.task}</Text>
-            <ActionIcon
-              color="blue"
-              variant="hover"
-              onClick={() => setTodos(todos.map((t, i) => {
-                if (i === index) {
-                  t.favourite = !todo.favourite;
-                }
-                return t
-              }))
-              }
-            >
-              <i className={`bi bi-star${todo.favourite ? "-fill" : ""}`} size={16} />
-              {todo.favourite}
-            </ActionIcon>
-            <ActionIcon
-              color="red"
-              variant="hover"
-              onClick={() => setTodos((t) => {
-                return t.filter((_, i) => i != index)
-              })
-              }
-            >
-              <i className={`bi bi-trash`} size={16} />
-              {todo.favourite}
-            </ActionIcon>
-          </Group>
+              <Text
+                sx={{ flex: 1 }}
+                onClick={() => { openEditMenu(index) }}
+              >{todo.task}</Text>
+              <ActionIcon
+                color="blue"
+                variant="hover"
+                onClick={() => starTodo(index)}
+              >
+                <i className={`bi bi-star${todo.favourite ? "-fill" : ""}`} size={16} />
+                {todo.favourite}
+              </ActionIcon>
+              <ActionIcon
+                color="red"
+                variant="hover"
+                onClick={() => deleteTodo(index)}
+              >
+                <i className={`bi bi-trash`} size={16} />
+                {todo.favourite}
+              </ActionIcon>
+            </Group>
+          </Paper>
         ))
       }
     </>
